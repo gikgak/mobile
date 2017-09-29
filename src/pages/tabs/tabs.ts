086d944c4a8188dde6c103d0f5bd4c8eac4e7829
Yann Bertrand <yann@bertrand.com>
2017-09-28 23:53:45 -0400

    Create Messaging, Calendar & Gaming tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { MessagingPage } from '../messaging/messaging';
+import { CalendarPage } from '../calendar/calendar';
+import { GamingPage } from '../gaming/gaming';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,8 +11,9 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = MessagingPage;
+  tab3Root = CalendarPage;
+  tab4Root = GamingPage;
 
   constructor() {
 
